10a1115a55a652917fa8e5dc2bf48825ff419217
Kevin Miller <kevinamiller77@gmail.com>
2020-03-24 00:32:45 -0400

    Prep with a make file

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+project "imgui"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imgui.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_widgets.cpp"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
